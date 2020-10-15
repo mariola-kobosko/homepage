1f472247dd4232f957ae510ec737e7362150fc88
Mariola Kobosko <mariola.kobosko@gmail.com>
2020-10-15 20:35:17 +0200

    more notes about CSS

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-
-const name = 'Mariola'
-const age = '32'
+const name = "Mariola";
+const age = "32";
 
 console.log(name);
 console.log(age);
-console.log(`Cześć! Nazywam się ${name} i mam ${age} lata.`)
+console.log(`Cześć! Nazywam się ${name} i mam ${age} lata.`);
