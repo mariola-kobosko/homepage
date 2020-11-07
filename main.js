163b8fbc1dd16eb5dcb3a55595b529ab2f02cad3
Mariola Kobosko <mariola.kobosko@gmail.com>
2020-11-07 13:14:58 +0100

    Add normalize file and BEM in css

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const age = "32";
 console.log(name);
 console.log(age);
 console.log(`Cześć! Nazywam się ${name} i mam ${age} lata.`);
+
+const heading = document.querySelector('.index-header');
+console.log(heading);
+const h2 = document.querySelector('.dom__js');
+console.log(h2);
+h2.innerHTML = `Ćwiczymy domowe przedszkole`
+
